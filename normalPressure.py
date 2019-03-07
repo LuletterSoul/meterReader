e8ba06d911ef228552b8ac2910eef6b3ecf281f4
XiangDe <qq313700046@icloud.com>
2019-03-07 16:41:04 +0800

    history

--- normalPressure.py
+++ normalPressure.py
@@ -1,5 +1,19 @@
 from Common import *
 import json
+import util.PlotUtil as plot
+
+plot_index = 0
+
+
+def inc():
+    global plot_index
+    plot_index += 1
+    return plot_index
+
+
+def reset():
+    global plot_index
+    plot_index = 0
 
 
 def normalPressure(image, info):
@@ -36,6 +50,7 @@ def readPressure(image, info):
     filtered_thresh = np.zeros(thresh.shape, dtype=np.uint8)
     cv2.drawContours(filtered_thresh, contours, -1, (255, 0, 0), thickness=cv2.FILLED)
     thresh = filtered_thresh
+    plot.subImage(src=filtered_thresh, index=inc(), title='Filtered Threshold', cmap='gray')
     # load meter calibration form configuration
     if 'ptrResolution' not in info:
         return -1
@@ -63,6 +78,13 @@ def readPressure(image, info):
                                                                patch_degree=0.5,
                                                                ptr_resolution=ptr_resolution, clean_ration=clean_ration)
     line_ptr = cv2PtrTuple2D(line_ptr)
+    plot.subImage(src=cv2.bitwise_or(thresh, pointer_mask), index=inc(), title='pointer', cmap='gray')
+    cv2.line(src, (start_ptr[0], start_ptr[1]), (center[0], center[1]), color=(0, 0, 255), thickness=1)
+    cv2.line(src, (end_ptr[0], end_ptr[1]), (center[0], center[1]), color=(0, 0, 255), thickness=1)
+    cv2.circle(src, (start_ptr[0], start_ptr[1]), 5, (0, 0, 255), -1)
+    cv2.circle(src, (end_ptr[0], end_ptr[1]), 5, (0, 0, 255), -1)
+    cv2.circle(src, (center[0], center[1]), 2, (0, 0, 255), -1)
+    plot.subImage(src=cv2.cvtColor(src, cv2.COLOR_BGR2RGB), index=inc(), title='calibration')
     start_value = info['startValue']
     total = info['totalValue']
     value = AngleFactory.calPointerValueByPoint(startPoint=start_ptr, endPoint=end_ptr,
@@ -103,3 +125,34 @@ def cv2PtrTuple2D(tuple):
     else:
         return np.array([0, 0])
     return tuple
+
+
+def readPressureValueFromDir(meter_id, img_dir, config):
+    img = cv2.imread(img_dir)
+    file = open(config)
+    info = json.load(file)
+    assert info is not None
+    info["template"] = cv2.imread("template/" + meter_id + ".jpg")
+    return readPressureValueFromImg(img, info)
+
+
+def readPressureValueFromImg(img, info):
+    if img is None:
+        raise Exception("Failed to resolve the empty image.")
+    return normalPressure(img, info)
+
+
+if __name__ == '__main__':
+    res1 = readPressureValueFromDir('lxd1_4', 'image/lxd1.jpg', 'config/lxd1_4.json')
+    res2 = readPressureValueFromDir('szk2_1', 'image/szk2.jpg', 'config/szk2_1.json')
+    res3 = readPressureValueFromDir('szk1_5', 'image/szk1.jpg', 'config/szk1_5.json')
+    res4 = readPressureValueFromDir('wn1_5', 'image/wn1.jpg', 'config/wn1_5.json')
+    res5 = readPressureValueFromDir('xyy3_1', 'image/xyy3.jpg', 'config/xyy3_1.json')
+    res6 = readPressureValueFromDir('pressure2_1', 'image/pressure2.jpg', 'config/pressure2_1.json')
+    plot.show()
+    print(res1)
+    print(res2)
+    print(res3)
+    print(res4)
+    print(res5)
+    print(res6)
