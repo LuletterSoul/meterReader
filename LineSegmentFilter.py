6d8ace5cdf5d70a5f5f2ea337488ba56f77d8ddb
XiangDe <qq313700046@icloud.com>
2019-03-20 09:55:37 +0800

    line matched

--- LineSegmentFilter.py
+++ LineSegmentFilter.py
@@ -98,7 +98,7 @@ def cleanExternalContours(src, output=None):
     return output
 
 
-def clean(src, new_src=None):
+def clean(src, area_thresh=500, approx_thresh=8, new_src=None):
     pyr = cv2.pyrDown(src)
     timg = cv2.pyrUp(pyr)
     new_src = np.zeros(src.shape[:2], dtype=np.uint8)
@@ -109,7 +109,7 @@ def clean(src, new_src=None):
     high = (max_area - min_area) * 0.7
     for i, contour in enumerate(contours):
         approx = cv2.approxPolyDP(contour, cv2.arcLength(contour, True) * 0.02, True)
-        if len(approx) <= 8 and cv2.contourArea(contour) < 500:
+        if len(approx) <= approx_thresh and cv2.contourArea(contour) < area_thresh:
             cv2.drawContours(new_src, [contour], -1, 255, cv2.FILLED)
     return new_src
 
@@ -130,70 +130,69 @@ def drawSquares(img, squares):
     cv2.imshow("Squares", img)
     cv2.waitKey(0)
 
-    def filter(binary, draw_src):
-        line_detector = cv2.createLineSegmentDetector()
-        # Returned Vec4i [x1,y1,x2,y2],(x1,y1) is the start,(x2,y2) is the end.
-        _lines, width, prec, nfa = line_detector.detect(binary)
-        line_descriptors = []
-        level_line_vector = np.array([0, 1])
-        for line in _lines:
-            line = line[0]
-            center = calCenter(line)
-            start_ptr = np.array([line[0], line[1]])
-            end_ptr = np.array([line[2], line[3]])
-            line_len = EuclideanDistance(start_ptr, end_ptr)  # distance between start point and end point
-            if line_len > 30:
-                continue
-            line_vector = calLineVector(line)  # line vector
-            line_descriptors.append(
-                np.array([start_ptr, end_ptr, line_vector, center, line_len]))  # compose line descriptors
 
-        match_pair = []
-        line_set_len = len(line_descriptors)
-        vis = np.zeros(shape=(line_set_len, 1), dtype=np.int32)
-        avg_center_dis = 0
-        avg_diff_angle = 0
-        avg_diff_len = 0
-        index_record = []
-        for i in range(0, line_set_len):
-            if vis[i] == 1:
-                continue
-            for j in range(0, line_set_len):
-                if i == j or vis[j] == 1:
-                    continue
-                t = cv2.getTickCount()
-                dis_center = EuclideanDistance(line_descriptors[i][3], line_descriptors[j][3])
-                hor_angle1 = AngleFactory.calAngleClockwiseByVector(level_line_vector, line_descriptors[i][2])
-                hor_angle2 = AngleFactory.calAngleClockwiseByVector(level_line_vector, line_descriptors[j][2])
-                # fix vector orientation in order to locate angle range in [0,pi]
-                if hor_angle1 >= 1.5 * np.pi:
-                    hor_angle1 = hor_angle1 - np.pi
-                if hor_angle2 >= 1.5 * np.pi:
-                    hor_angle2 = hor_angle2 - np.pi
-                diff_angle = np.rad2deg(np.abs(hor_angle2 - hor_angle1))
-                # print("Count: " + str(i * j) + " : " + str(diff_angle))
-                diff_len = np.abs(line_descriptors[i][4] - line_descriptors[j][4])
-                # avg_center_dis += dis_center
-                # avg_diff_angle += diff_angle
-                # avg_diff_len += diff_len
-                n = (cv2.getTickCount() - t) / cv2.getTickFrequency()
-                # print("Execution Time of Per iteration: ", n)
-                if dis_center < thresh1 and diff_len < thresh2 and diff_angle < thresh3:
-                    match_pair.append(_lines[i])
-                    match_pair.append(_lines[j])
-                    vis[i] = 1
-                    vis[j] = 1
-                    end = (cv2.getTickCount() - t) / cv2.getTickFrequency()
-                    print("Execution Time of Per match iteration:", end)
-                    break
+def filter(_lines, draw_src):
+    line_detector = cv2.createLineSegmentDetector()
+    line_descriptors = []
+    level_line_vector = np.array([0, 1])
+    for line in _lines:
+        line = line[0]
+        center = calCenter(line)
+        start_ptr = np.array([line[0], line[1]])
+        end_ptr = np.array([line[2], line[3]])
+        line_len = EuclideanDistance(start_ptr, end_ptr)  # distance between start point and end point
+        if line_len > 30:
+            continue
+        line_vector = calLineVector(line)  # line vector
+        line_descriptors.append(
+            np.array([start_ptr, end_ptr, line_vector, center, line_len]))  # compose line descriptors
 
-        avg_div = line_set_len * (line_set_len - 1)
-        print("Avg center distance:", avg_center_dis / avg_div)
-        print("Avg ang:", avg_diff_angle / avg_div)
-        print("Avg len:", avg_diff_len / avg_div)
-        #  print([np.array([l[0][0], l[1][1], l[1][0], l[1][1]]) for l in match_pair if len(l) > 0])
-        line_detector.drawSegments(draw_src, np.array(match_pair))
-        # line_detector.drawSegments(draw_src, _lines)
+    match_pair = []
+    line_set_len = len(line_descriptors)
+    vis = np.zeros(shape=(line_set_len, 1), dtype=np.int32)
+    avg_center_dis = 0
+    avg_diff_angle = 0
+    avg_diff_len = 0
+    index_record = []
+    for i in range(0, line_set_len):
+        if vis[i] == 1:
+            continue
+        for j in range(0, line_set_len):
+            if i == j or vis[j] == 1:
+                continue
+            t = cv2.getTickCount()
+            dis_center = EuclideanDistance(line_descriptors[i][3], line_descriptors[j][3])
+            hor_angle1 = AngleFactory.calAngleClockwiseByVector(level_line_vector, line_descriptors[i][2])
+            hor_angle2 = AngleFactory.calAngleClockwiseByVector(level_line_vector, line_descriptors[j][2])
+            # fix vector orientation in order to locate angle range in [0,pi]
+            if hor_angle1 >= 1.5 * np.pi:
+                hor_angle1 = hor_angle1 - np.pi
+            if hor_angle2 >= 1.5 * np.pi:
+                hor_angle2 = hor_angle2 - np.pi
+            diff_angle = np.rad2deg(np.abs(hor_angle2 - hor_angle1))
+            # print("Count: " + str(i * j) + " : " + str(diff_angle))
+            diff_len = np.abs(line_descriptors[i][4] - line_descriptors[j][4])
+            # avg_center_dis += dis_center
+            # avg_diff_angle += diff_angle
+            # avg_diff_len += diff_len
+            n = (cv2.getTickCount() - t) / cv2.getTickFrequency()
+            # print("Execution Time of Per iteration: ", n)
+            if dis_center < thresh1 or diff_angle < thresh3:
+                match_pair.append(_lines[i])
+                match_pair.append(_lines[j])
+                vis[i] = 1
+                vis[j] = 1
+                # end = (cv2.getTickCount() - t) / cv2.getTickFrequency()
+                # print("Execution Time of Per match iteration:", end)
+                break
+
+    avg_div = line_set_len * (line_set_len - 1)
+    # print("Avg center distance:", avg_center_dis / avg_div)
+    # print("Avg ang:", avg_diff_angle / avg_div)
+    # print("Avg len:", avg_diff_len / avg_div)
+    #  print([np.array([l[0][0], l[1][1], l[1][0], l[1][1]]) for l in match_pair if len(l) > 0])
+    line_detector.drawSegments(draw_src, np.array(match_pair))
+    # line_detector.drawSegments(draw_src, _lines)
 
 
 if __name__ == '__main__':
