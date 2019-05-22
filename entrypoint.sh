5aa02332001b8a2879036a4f26f76243d229fea6
XiangDe <qq313700046@icloud.com>
2019-05-22 23:03:02 +0800

    use prod settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ls
-cd meterReader
-python ./server/manage.py makemigrations makemigrations ins --setting=server.settings.prod
+#cd meterReader
+python ./server/manage.py makemigrations ins --setting=server.settings.prod
 python ./server/manage.py migrate ins --setting=server.settings.prod
-python ./server/manage.py --setting=server.settings.prod 0.0.0.8080
+python ./server/manage.py --setting=server.settings.prod 0.0.0.0:8080
