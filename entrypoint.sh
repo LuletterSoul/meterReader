cfc0e4d19ab6fc5d9321e8d5d86830e4adbfe8ee
XiangDe <qq313700046@icloud.com>
2019-05-22 22:17:22 +0800

    use prod settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python ./server/manage.py makemigrations ins
-python ./server/manage.py migrate ins
+python ./server/manage.py makemigrations makemigrations ins --setting=server.settings.prod
+python ./server/manage.py migrate ins --setting=server.settings.prod
 python ./server/manage.py --setting=server.settings.prod 0.0.0.8080
